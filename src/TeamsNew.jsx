274097ada867d505542ff4c5e229755b0d9e76ef
Makayla <makaylabusher@gmail.com>
2023-01-17 11:35:27 -0600

    TeamsNew form

---
+++ src/TeamsNew.jsx
@@ -0,0 +1,21 @@
+export function TeamsNew() {
+  return (
+    <div>
+      <h1>New Team</h1>
+      <form>
+        <div>
+          Name: <input name="name" type="text" />
+        </div>
+        <div>
+          Location: <input name="location" type="text" />
+        </div>
+        <div>
+          League: <input name="league" type="text" />
+        </div>
+        <div>
+          Logo: <input name="logo" type="text" />
+        </div>
+      </form>
+    </div>
+  );
+}
